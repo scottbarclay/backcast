4b1196b108a2b7a1720d4682ce52089b0f1e2625
Scott Barclay <scottdbarclay@gmail.com>
2018-01-30 11:47:00 -0800

    Sync with master

--- src/views/app.js
+++ src/views/app.js
@@ -1,17 +1,26 @@
 var AppView = Backbone.View.extend({
-
   el: '#app',
-
+  template: templateURL('src/templates/app.html'),
+  collection: new Videos(exampleVideoData),
   initialize: function() {
-    this.videos = new Videos();
+    //this.videos = new Videos(exampleVideoData);
+    this.render();
   },
-
 
   render: function() {
     this.$el.html(this.template());
+    this.search = new SearchView({
+    	el: this.$el.html('.search'),
+    	collection: this.collection
+    }).render();
+    this.videoList = new VideoListView({
+    	el: this.$el.html('.videoList'),
+    	collection: this.collection
+    }).render();
+    this.videoPlayer = new VideoPlayerView({
+    	el: this.$el.html('.videoPlayer'),
+    	collection: this.collection
+    }).render();
     return this;
   },
-
-  template: templateURL('src/templates/app.html')
-
 });
