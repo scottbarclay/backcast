3866ba37eb96b6ec477d1362cb892e7219c5aca1
Scott Barclay <scottdbarclay@gmail.com>
2018-01-30 11:56:04 -0800

    Resync with master

--- src/views/app.js
+++ src/views/app.js
@@ -3,19 +3,12 @@ var AppView = Backbone.View.extend({
   template: templateURL('src/templates/app.html'),
   collection: new Videos(exampleVideoData),
   initialize: function() {
-<<<<<<< HEAD
     //this.videos = new Videos(exampleVideoData);
-=======
-    //collection of videos
-    this.videos = new Videos();
->>>>>>> 8acd4f5ca589498bd2cb05ef7fbb6d38c83cbe95
     this.render();
   },
 
   render: function() {
-    //this is the html div with the id of App
     this.$el.html(this.template());
-<<<<<<< HEAD
     this.search = new SearchView({
     	el: this.$el.html('.search'),
     	collection: this.collection
@@ -28,13 +21,6 @@ var AppView = Backbone.View.extend({
     	el: this.$el.html('.videoPlayer'),
     	collection: this.collection
     }).render();
-=======
-
-    new VideoPlayerView().render();
-    new SearchView().render();
-    new VideoListView().render(); 
-
->>>>>>> 8acd4f5ca589498bd2cb05ef7fbb6d38c83cbe95
     return this;
   },
 });
